2315356f0faf748ad7914668bafc20b9410e42ce
Ben Rowe <browe@switch.tv>
2017-06-26 09:09:15 +1000

    Add SiteServiceInjection middleware

---
+++ app/Http/Middleware/SiteServiceInjection.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Entity\Site;
+use Closure;
+use App\Repositories\Contracts\SiteRepository;
+
+/**
+ * Site Service Injection Middleware
+ *
+ * Detects the requested site and loads in the configured providers
+ *
+ * @package App\Http\Middleware
+ */
+class SiteServiceInjection
+{
+    private $site;
+
+    /**
+     * SiteServiceInjection constructor.
+     *
+     * @param SiteRepository $site
+     */
+    public function __construct(SiteRepository $site)
+    {
+        $this->site = $site;
+    }
+
+    /**
+     * @param         $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $site = $this->getSite($request);
+        if ($site) {
+            foreach ($site->getConfigValue('providers', []) as $serviceName => $object) {
+                /*app($serviceName)->bind(function () use ($object) {
+                    return new $object;
+                });*/
+            }
+        } else {
+            // unable to load site!
+            abort(404);
+        }
+        return $next($request);
+    }
+
+    /**
+     * Retrieve an instance of the site based on the current request
+     *
+     * note: other siteId detection methods could be used here, such as GET, POST, headers, etc
+     *
+     * @param $request
+     * @return Site|null
+     */
+    private function getSite($request): ?Site
+    {
+        $siteId = (int)$request->route()[2]['siteId'];
+
+        return $this->site->find($siteId);
+    }
+}
